8a91f6b4090121dd65f74a38ddfaf29d936e638b
Christian Finel <macreiben@gmail.com>
2024-11-18 08:24:04 +0100

    initial version

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Hello from "./hello";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Hello />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/hello.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const Hello: React.FC = () => {
+  const [message, setMessage] = useState<string>("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://demo-api-node-raccoon-gygyh7dwbnhpadgy.westeurope-01.azurewebsites.net/hello"
+        );
+        setMessage(response.data.message);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>{message}</h1>
+    </div>
+  );
+};
+
+export default Hello;
